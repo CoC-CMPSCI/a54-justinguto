af8cc6cb1f077db40b1d38fb3c6024e30f8d4d36
justinguto <jutogustowski@gmail.com>
2026-03-29 12:18:59 -0700

    Fix a54 prime numbers program

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
-//	Lab 5-11 : Problem Definition
-//	Make a program that find the prime numbers in the given range.
-//		example) find the prime numbers in the range [1..100]
-//		2, 3, 5, 7, ... , 97
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int begin, end;
+    int begin, end;
 
-	do
-	{
-		cout << "Enter your range [from, to] \n";
-		cin >> begin >> end;
-	} while (begin >= end || begin < 2 || end < 0);
+    do
+    {
+        cout << "Enter your range [from, to] \n";
+        cin >> begin >> end;
+    } while (begin >= end || begin < 2 || end < 2);
 
-	// TODO: iterate through each number in the range [begin..end]
-	// TODO: check if n is prime by testing divisibility from 2 up to n-1
-	// TODO: if n is divisible by i, it is not prime
-	// TODO: if n is prime, print it
+    for (int n = begin; n <= end; n++)
+    {
+        bool isPrime = true;
 
-	cout << endl;
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
 
-	return 0;
+        if (isPrime)
+        {
+            cout << n << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
 }
